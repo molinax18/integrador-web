3ddcee61e3e7b601f9606d6adc5eee274c78a561
molinax18 <arielmolina8953@gmail.com>
2024-11-07 14:31:41 -0300

    tarjetas emergentes hechas

--- js/main.js
+++ js/main.js
@@ -92,14 +92,15 @@ form.addEventListener("submit", (e) => {
   
   const { nombre, apellido, correo } = errores;
 
-  if (nombre || apellido || correo) {
-    alert("Los datos no son correctos, intenta de nuevo");
-  } else {
-    alert("El formulario se ha enviado correctamente");
-    inputs.forEach(inp => {
-      inp.value = "";
-    });
+  if (nombre || apellido || correo) return;
+
+  const info = {
+    nombre: e.target.nombre.value,
+    apellido: e.target.apellido.value,
+    correo: e.target.correo.value,
   }
+
+  modal(info);
 })
 
 function mostrarErrores(inpHeader, errorMsj) {
@@ -111,4 +112,57 @@ function mostrarErrores(inpHeader, errorMsj) {
   
   inpHeader.querySelector("span").textContent = errorMsj;
   inpHeader.querySelector("label").classList.add("error-input");
+}
+
+function modal(info) {
+  const { nombre, apellido, correo } = info;
+  
+  const elemPadre = document.createElement("div");
+  elemPadre.classList.add("modal-container");
+
+  const modal = document.createElement("section");
+  modal.classList.add("modal");
+  modal.innerHTML = `
+    <header>
+      <h3>Contacto</h3>
+      <p>¿Quieres enviar estos datos de contacto?</p>
+    </header>
+    <div class="contenido-modal">
+      <article>
+        <h4>Nombre completo</h4>
+        <p>${nombre} ${apellido}</p>
+      </article>
+      <article>
+        <h4>Correo electrónico</h4>
+        <p>${correo}</p>
+      </article>
+    </div>
+    <footer>
+      <button onclick="enviarForm()">Enviar</button>
+      <button onclick="volverForm()">Volver</button>
+    </footer>
+  `
+  document.body.appendChild(elemPadre);
+  elemPadre.appendChild(modal);
+}
+
+function cardInfo(mensaje) {
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+  cardInfo.innerHTML = `
+    <p>${mensaje}</p>
+  `
+
+  document.body.appendChild(cardInfo);
+  setTimeout(() => cardInfo.remove(), 2500);
+}
+
+function enviarForm() {
+  document.querySelector(".modal-container").remove();
+  inputs.forEach(inp => inp.value = "");
+  cardInfo("¡Se ha enviado el formulario!");
+}
+
+function volverForm() {
+  document.querySelector(".modal-container").remove();
 }
